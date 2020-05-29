3e478d4d99fe9be6967eb8e4198791679570c26a
Aaron Lee <63542621+aaronLab@users.noreply.github.com>
2020-05-29 19:28:55 +0900

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,9 +3,9 @@ export const login = (req, res) => res.render("login", { pageTitle: "Login" });
 export const logout = (req, res) =>
   res.render("logout", { pageTitle: "Logout" });
 export const users = (req, res) => res.render("users", { pageTitle: "Users" });
-export const userDetail = (req, res) =>
-  res.render("userDetail", { pageTitle: "User Detail" });
 export const editProfile = (req, res) =>
   res.render("editProfile", { pageTitle: "Edit Profile" });
 export const changePassword = (req, res) =>
   res.render("changePassword", { pageTitle: "Change Password" });
+export const userDetail = (req, res) =>
+  res.render("userDetail", { pageTitle: "User Detail" });
