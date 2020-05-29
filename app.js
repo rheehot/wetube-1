94430bbe6051cd6d335ae2d29425cc40b6646979
Aaron Lee <63542621+aaronLab@users.noreply.github.com>
2020-05-29 19:28:48 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
 import "core-js";
-import express from "express";
-import morgan from "morgan";
-import helmet from "helmet";
-import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import { localMiddleware } from "./middlewares";
+import cookieParser from "cookie-parser";
+import express from "express";
+import globalRouter from "./routers/globalRouter";
+import helmet from "helmet";
+
+import morgan from "morgan";
 import routes from "./routes";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
-import globalRouter from "./routers/globalRouter";
+import { localMiddleware } from "./middleware";
 
 const app = express();
 
 app.use(helmet());
 app.set("view engine", "pug");
-app.use(cookieParser());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 app.use(localMiddleware);
 
 app.use(routes.home, globalRouter);
