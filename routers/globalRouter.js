0f372f2c5bde85d59ee7d293be944560734b4688
Aaron Lee <63542621+aaronLab@users.noreply.github.com>
2020-05-30 22:15:21 +0900

    Update globalRouter.js

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -3,9 +3,10 @@ import routes from "../routes";
 import { home, search } from "../controllers/videoController";
 import {
   getJoin,
-  login,
   logout,
   postJoin,
+  getLogin,
+  postLogin,
 } from "../controllers/userController";
 
 const globalRouter = express.Router();
@@ -14,7 +15,10 @@ globalRouter.get(routes.join, getJoin);
 globalRouter.post(routes.join, postJoin);
 
 globalRouter.get(routes.home, home);
-globalRouter.get(routes.login, login);
+
+globalRouter.get(routes.login, getLogin);
+globalRouter.post(routes.login, postLogin);
+
 globalRouter.get(routes.logout, logout);
 globalRouter.get(routes.search, search);
 
