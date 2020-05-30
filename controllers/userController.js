bcfa2e887d42250383a6136914287566ee1cb09e
Aaron Lee <63542621+aaronLab@users.noreply.github.com>
2020-05-30 22:15:15 +0900

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -18,9 +18,17 @@ export const postJoin = (req, res) => {
   }
 };
 
-export const login = (req, res) => res.render("login", { pageTitle: "Login" });
-export const logout = (req, res) =>
-  res.render("logout", { pageTitle: "Logout" });
+export const getLogin = (req, res) =>
+  res.render("login", { pageTitle: "Login" });
+export const postLogin = (req, res) => {
+  res.redirect(routes.home);
+};
+
+export const logout = (req, res) => {
+  // To Do: Process Log Out
+  res.redirect(routes.home);
+};
+
 export const users = (req, res) => res.render("users", { pageTitle: "Users" });
 export const editProfile = (req, res) =>
   res.render("editProfile", { pageTitle: "Edit Profile" });
