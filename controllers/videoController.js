eff364fc7700fdec5eb921ccde177e22e77b58fd
Aaron Lee <63542621+aaronLab@users.noreply.github.com>
2020-05-30 22:15:18 +0900

    Update videoController.js

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,4 +1,5 @@
 import { videos } from "../db";
+import routes from "../routes";
 
 export const home = (req, res) => {
   res.render("home", { pageTitle: "Home", videos });
@@ -14,8 +15,16 @@ export const search = (req, res) => {
 // export const videos = (req, res) =>
 //   res.render("videos", { pageTitle: "Videos" });
 
-export const upload = (req, res) =>
+export const getUpload = (req, res) =>
   res.render("upload", { pageTitle: "Upload" });
+
+export const postUpload = (req, res) => {
+  const {
+    body: { file, title, description },
+  } = req;
+  // To Do: Upload and Save Video
+  res.redirect(routes.videoDetail(123123));
+};
 
 export const videoDetail = (req, res) =>
   res.render("videoDetail", { pageTitle: "Video Detail" });
