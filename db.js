54b2c5e103ac35625d0e5486eaf5a07741caf00a
Aaron Lee <63542621+aaronLab@users.noreply.github.com>
2020-05-30 20:09:17 +0900

    Create db.js

---
+++ db.js
@@ -0,0 +1,39 @@
+// fake db for dev
+export const videos = [
+  {
+    id: 123123,
+    title: "dolorem et sint",
+    description: "Et sint natus omnis.",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 121212,
+      name: "Aaron",
+      email: "Karli.Sporer@example.net",
+    },
+  },
+  {
+    id: 234234,
+    title: "veritatis",
+    description: "In tenetur nobis aperiam.",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 121212,
+      name: "Aaron",
+      email: "Karli.Sporer@example.net",
+    },
+  },
+  {
+    id: 345345,
+    title: "Dolores incidunt voluptatem.",
+    description: "Architecto vero aut asperiores ut suscipit.",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 121212,
+      name: "Aaron",
+      email: "Karli.Sporer@example.net",
+    },
+  },
+];
