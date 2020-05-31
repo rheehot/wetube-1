f0db25b1b2b35bce13793167e2fb8eb850a7e892
Aaron Lee <63542621+aaronLab@users.noreply.github.com>
2020-05-31 16:32:45 +0900

    Update db.js

--- db.js
+++ db.js
@@ -1,42 +1,17 @@
-// fake db for dev
-export const videos = [
-  {
-    id: 123123,
-    title: "dolorem et sint",
-    description: "Et sint natus omnis.",
-    views: 24,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 121212,
-      name: "Aaron",
-      email: "Karli.Sporer@example.net",
-    },
-  },
-  {
-    id: 234234,
-    title: "veritatis",
-    description: "In tenetur nobis aperiam.",
-    views: 24,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 121212,
-      name: "Aaron",
-      email: "Karli.Sporer@example.net",
-    },
-  },
-  {
-    id: 345345,
-    title: "Dolores incidunt voluptatem.",
-    description: "Architecto vero aut asperiores ut suscipit.",
-    views: 24,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 121212,
-      name: "Aaron",
-      email: "Karli.Sporer@example.net",
-    },
-  },
-];
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+});
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log(`✅ Connected DB\n\n\n`);
+
+const handleError = () => console.log(`🖕 Error on DB\n\n\n`);
+
+db.once("open", handleOpen);
+db.on("error", handleError);
